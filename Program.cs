389498563b4668126d62c43b41be5674ad5a6eba
Aaqib Ali <75785706+Aaqibali1@users.noreply.github.com>
2023-06-28 04:27:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string aFriend = "Aaqib ali";
+        Console.WriteLine($"Hello {aFriend} ");
+        string firstFriend = "Aaqib Ali";
+        string secondFriend = "Ayaz Ali";
+        Console.WriteLine($" My Friends Are {firstFriend} and {secondFriend} ");
+
+        Console.WriteLine($" The name {firstFriend} has {firstFriend.Length} letters ");
+
+        Console.WriteLine($" The name {secondFriend} has {secondFriend.Length} letters ");
+    }
+}
