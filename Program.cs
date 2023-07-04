a3cbd6c0781339635fe0d69d9994d0558c3770b8
Aaqib Ali <75785706+Aaqibali1@users.noreply.github.com>
2023-07-04 16:29:37 +0500

    strings

--- Program.cs
+++ Program.cs
@@ -2,15 +2,90 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        string aFriend = "Aaqib ali";
-        Console.WriteLine($"Hello {aFriend} ");
-        string firstFriend = "Aaqib Ali";
-        string secondFriend = "Ayaz Ali";
-        Console.WriteLine($" My Friends Are {firstFriend} and {secondFriend} ");
+        //Console.WriteLine("Hello, World!");
+        //string aFriend = "Aaqib ali";
+        //Console.WriteLine($"Hello {aFriend} ");
+        //string firstFriend = "Aaqib Ali";
+        //string secondFriend = "Ayaz Ali";
+        //Console.WriteLine($" My Friends Are {firstFriend} and {secondFriend} ");
 
-        Console.WriteLine($" The name {firstFriend} has {firstFriend.Length} letters ");
+        //Console.WriteLine($" The name {firstFriend} has {firstFriend.Length} letters ");
 
-        Console.WriteLine($" The name {secondFriend} has {secondFriend.Length} letters ");
+        //Console.WriteLine($" The name {secondFriend} has {secondFriend.Length} letters ");
+
+        //string greetings = "    Hello World!     ";
+        //Console.WriteLine($"[{greetings}]");
+
+
+        //string trimmedGreeting = greetings.TrimStart();
+        //Console.WriteLine($"[{trimmedGreeting}]");
+        //trimmedGreeting = greetings.TrimEnd();
+        //Console.WriteLine($"[{trimmedGreeting}]");
+
+
+        //string sayHello = "Hello World!";
+        //Console.WriteLine(sayHello);
+
+        //sayHello = sayHello.Replace("Hello", "Aaqib Ali");
+        //Console.WriteLine(sayHello);
+
+        //Console.WriteLine(sayHello.ToUpper());
+        //Console.WriteLine(sayHello.ToLower());
+
+        // string firstName;
+        // firstName = "AaQib Ali Sahito";
+        // Console.WriteLine(firstName);
+
+
+        // string secondName;
+        // secondName = "bob";
+        // Console.WriteLine(secondName);
+
+
+        // string firstName;
+        // firstName = "Bob";
+        // Console.WriteLine(firstName);
+        // firstName = "Liem";
+        // Console.WriteLine(firstName);
+        // firstName = "Isabella";
+        // Console.WriteLine(firstName);
+        // firstName = "Yasmin";
+        // Console.WriteLine(firstName);
+
+
+        // var message = "Hello world!";
+        // Console.WriteLine(message);
+
+
+        string message = "Bob!";
+        int x = 3;
+        decimal y = 34.4m;
+        Console.WriteLine($"Hello, {message} You have {x} in your inbox. The temperature is {y} celsius.");
+
+        string name = "Bob";
+        int messages = 3;
+        decimal temperature = 34.4m;
+
+        Console.Write("Hello, ");
+        Console.Write(name);
+        Console.Write("! You have ");
+        Console.Write(messages);
+        Console.Write(" in your inbox. The temperature is ");
+        Console.Write(temperature);
+        Console.Write(" celsius.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
